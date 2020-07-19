5726a16e73ffd11b272492737248880ebe70d20d
e-leal <leal.elyssa03@gmail.com>
2020-07-19 11:46:19 -0500

    Add user prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,11 @@
 // array of questions for user
 const questions = [
-
+"Please enter project title: ",
+"Please enter a project description: ",
+"Please enter installation instructions: ",
+"Please enter usage information: ",
+"Please enter contrinbution guidelines: ",
+"Please enter test instructions: "
 ];
 
 // function to write README file
